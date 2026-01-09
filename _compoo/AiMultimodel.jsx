dc740fb973ff26a71640d4abdc158ffcb9c7f276
AftekharHabib <rouhithmini1@gmail.com>
2026-01-09 21:55:08 +0600

    user preferace save korar kaj _eir kaj eikhan theke suru hobe

--- _compoo/AiMultimodel.jsx
+++ _compoo/AiMultimodel.jsx
@@ -14,6 +14,7 @@ import { Switch } from "@/components/ui/switch";
 import { MessageSquare, Lock } from "lucide-react"; 
 import { Button } from "@/components/ui/button";
 
+
 function AiMultimodel() {
   const [aiModeList, setAiModelList] = React.useState(AiModelList);
 
@@ -25,6 +26,9 @@ function AiMultimodel() {
     );
   };
 
+  // Firestore আপডেট করার লজিক
+
+  
   return (
     <div className="flex w-full h-[75vh] border-b overflow-x-auto">
       {aiModeList.map((model, index) => (
